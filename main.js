5c221bddad810b02e0bbe10c03d01b87ffb585f5
Shady-91 <Shady_91@hotmail.it>
2025-07-10 16:06:05 +0200

    finished

--- main.js
+++ main.js
@@ -7,5 +7,19 @@
 
 
 for (let i = 1; i <= 100; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+      } else if (i % 3 === 0) {
+            console.log("Fizz");
+      } else if (i % 5 === 0) {
+            console.log("Buzz");
+      } else {
+            console.log(i);
+      }
+}
 
-})
+// Questo codice utilizza un ciclo for da 1 a 100
+// if per verificare se il numero corrente è un multiplo di 3, 5 o entrambi.
+// Se il numero è un multiplo di 3 e 5, stampa "FizzBuzz"
+// Se il numero è un multiplo di 3, stampa "Fizz"
+// Se il numero è un multiplo di 5, stampa "Buzz"
