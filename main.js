00970418e44eb380b363a4f924b9ddf9a6baec02
Shady-91 <Shady_91@hotmail.it>
2025-07-10 15:49:18 +0200

    start

---
+++ main.js
@@ -0,0 +1,11 @@
+//scrivi un programma che stampa i numeri da 1 a 100 ma per i multipli di 3 stampi "Fizz";
+// per i multipli di 5 stampi "Buzz";
+// per i multipli di 3 e 5 stampi "FizzBuzz".
+
+
+
+
+
+for (let i = 1; i <= 100; i++) {
+
+})
